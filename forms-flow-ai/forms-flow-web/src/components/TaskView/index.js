ec135a9b259859b9a49d9ff4f915159e1d5361a9
Devin Bushey <devin.bushey@aot-technologies.com>
2022-06-23 15:21:15 -0700

    modified TaskView to render data on page load

--- forms-flow-ai/forms-flow-web/src/components/TaskView/index.js
+++ forms-flow-ai/forms-flow-web/src/components/TaskView/index.js
@@ -1,39 +1,62 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import { useDispatch, useSelector } from "react-redux";
+import { ALL_TASKS } from "../ServiceFlow/constants/taskConstants";
+import { setSelectedBPMFilter } from "../../actions/bpmTaskActions";
 
 import ServiceFilter from "./ServiceFilter";
 import TaskFilter from "./TaskFilter";
 import TaskTable from "./TaskTable";
 
-const TaskView = React.memo(() => {
+export default React.memo(() => {
 
-    return (
-        <div className="container" id="main">
+  const dispatch = useDispatch();
+  const filterList = useSelector((state) => state.bpmTasks.filterList);
+  const isFilterLoading = useSelector(
+    (state) => state.bpmTasks.isFilterLoading
+  );
+  const selectedFilter = useSelector((state) => state.bpmTasks.selectedFilter);
 
-            {/* Task Title */}
-            <div className="flex-container">
-                <div className="flex-item-left">
-                    <div style={{ display: "flex" }}>
-                        <h3 className="task-head" style={{ marginTop: "3px" }}>
-                            <i className="fa fa-cogs" aria-hidden="true" />
-                        </h3>
-                        <h3 className="task-head">
-                            {" "}
-                            <span className="forms-text" style={{ marginLeft: "1px" }}>
-                                Tasks
-                            </span>
-                        </h3>
-                    </div>
-                </div>
-            </div>
+  useEffect(() => {
+    if (!isFilterLoading && filterList.length && !selectedFilter) {
+      let filterSelected;
+      if (filterList.length > 1) {
+        filterSelected = filterList.find((filter) => filter.name === ALL_TASKS);
+        if (!filterSelected) {
+          filterSelected = filterList[0];
+        }
+      } else {
+        filterSelected = filterList[0];
+      }
+      dispatch(setSelectedBPMFilter(filterSelected));
+    }
+  }, [filterList, isFilterLoading, selectedFilter, dispatch]);
 
-            <ServiceFilter/>
-
-            <TaskFilter/>
-
-            <TaskTable/>
-
+  return (
+    <div className="container" id="main">
+      {/* Task Title */}
+      <div className="flex-container">
+        <div className="flex-item-left">
+          <div style={{ display: "flex" }}>
+            <h3 className="task-head" style={{ marginTop: "3px" }}>
+              <i className="fa fa-cogs" aria-hidden="true" />
+            </h3>
+            <h3 className="task-head">
+              {" "}
+              <span className="forms-text" style={{ marginLeft: "1px" }}>
+                Tasks
+              </span>
+            </h3>
+          </div>
         </div>
-    );
-}
-);
-export default TaskView;
+      </div>
+
+      <ServiceFilter />
+
+      <TaskFilter />
+
+      <TaskTable />
+
+    </div>
+  );
+});
